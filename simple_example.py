94991994d515520558c16717ea41ce9f7075178f
rocketpy <dimitr.ushakov@gmail.com>
2020-07-29 23:29:10 +0300

    updated

--- simple_example.py
+++ simple_example.py
@@ -10,3 +10,16 @@ df = pd.read_csv('file_name.csv')
 
 # if need remove two data points with notes
 new_df = df[df.notes.astype(str) == 'nan'].reset_index(drop=True)
+
+#  print distribution of experience levels
+new_df.some_column_name.value_counts()
+
+#  to combine  groups
+comb_df = new_df.copy()
+comb_df['some_column'] = comb_df.column_name == 'column_name'
+
+#  for make varieties categorical
+vars = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+comb_df['favorite'] = pd.Categorical(comb_df['favorite'], categories = varieties)
+comb_df['favorites'] = pd.Categorical(comb_df['favorites'], categories = varieties)
+
