2671f9a9f7e36c2379ca5ee8e154d92f14ba1655
rocketpy <dimitr.ushakov@gmail.com>
2020-07-29 23:17:24 +0300

    updated

--- simple_example.py
+++ simple_example.py
@@ -1,1 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#  for jupyter notebook use:  %matplotlib inline
 
+
+#  data preparation
+df = pd.read_csv('file_name.csv')
+# print(df)
+
+# if need remove two data points with notes
+new_df = df[df.notes.astype(str) == 'nan'].reset_index(drop=True)
