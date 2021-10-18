506d22e48f383537a74ef720b1b2cf7297be8707
rocketpy <dimitr.ushakov@gmail.com>
2021-10-18 22:10:14 +0300

    Update Combining_data.py

--- Combining_data.py
+++ Combining_data.py
@@ -1,1 +1,17 @@
+# Combining data with merge(), .join(), concat()
 
+import pandas as pd
+
+
+temp_data = pd.read_csv("temp.csv")
+time_data = pd.read_csv("time.csv")
+
+print(temp_data.head())
+print(time_data.head())
+
+# in Jupyter Notebook
+# temp_data.head()
+# time_data.head()
+
+# temp_data.shape
+# time_data.shape
